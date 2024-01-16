5aa62cccdc522353b3582648e951449497ae6080
Ihor Hranetskyi <i.hranetskyi@gmail.com>
2024-01-16 22:45:02 +0200

    env variables were added to the db-config file

--- routes/Clubs.js
+++ routes/Clubs.js
@@ -2,43 +2,39 @@ const express = require('express');
 const router = express.Router();
 const { Club, Player, Position, Country } = require('../models');
 
-router.get('/test', async (req, res) => {
-  res.json([1,2,3]);
+router.get('/', async (req, res) => {
+  const clubs = await Club.findAll({
+    order: [['name', 'ASC']],
+  });
+
+  res.json(clubs);
 });
 
-// router.get('/', async (req, res) => {
-//   const clubs = await Club.findAll({
-//     order: [['name', 'ASC']],
-//   });
+router.get('/:club', async (req, res) => {
+  const clubName = req.params.club;
+  const club = await Club.findOne({
+    where: { slug: clubName },
+  });
 
-//   res.json(clubs);
-// });
+  res.json(club);
+});
 
-// router.get('/:club', async (req, res) => {
-//   const clubName = req.params.club;
-//   const club = await Club.findOne({
-//     where: { slug: clubName },
-//   });
+router.get('/:club/players', async (req, res) => {
+  const clubName = req.params.club;
+  const club = await Club.findOne({
+    where: { slug: clubName },
+  });
 
-//   res.json(club);
-// });
+  const players = await Player.findAll({
+    where: { clubId: club.id },
+    include: [
+      { model: Club },
+      { model: Position },
+      { model: Country },
+    ],
+  });
 
-// router.get('/:club/players', async (req, res) => {
-//   const clubName = req.params.club;
-//   const club = await Club.findOne({
-//     where: { slug: clubName },
-//   });
-
-//   const players = await Player.findAll({
-//     where: { clubId: club.id },
-//     include: [
-//       { model: Club },
-//       { model: Position },
-//       { model: Country },
-//     ],
-//   });
-
-//   res.json(players);
-// });
+  res.json(players);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,22 +7,18 @@ const port = process.env.PORT || 2000;
 app.use(express.json());
 app.use(cors());
 
-// const DB = require('./models');
+const DB = require('./models');
 
 // Routers
 const clubsRouter = require('./routes/Clubs');
 app.use('/clubs', clubsRouter);
 
-// DB.sequelize.sync()
-//   .then(() => {
-//     app.listen(port, () => {
-//       console.log(`Example app listening on port ${port}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error('Error syncing database:', error.message);
-//   });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+DB.sequelize.sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error.message);
+  });
